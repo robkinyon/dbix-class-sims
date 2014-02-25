5877d47f4e5184b7b86ca0af4f26dbc83979f95f
Rob Kinyon <rob.kinyon@gmail.com>
2014-02-25 11:47:31 -0500

    Increment version number

--- lib/DBIx/Class/Sims.pm
+++ lib/DBIx/Class/Sims.pm
@@ -12,7 +12,7 @@ use List::Util qw( shuffle );
 use Scalar::Util qw( reftype );
 use String::Random qw( random_regex );
 
-our $VERSION = '0.200010';
+our $VERSION = '0.200020';
 
 # Guarantee that toposort is loaded.
 use base 'DBIx::Class::TopoSort';
